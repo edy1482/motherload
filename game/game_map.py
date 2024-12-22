6a130f979bf48a5d08bc6a72d36fae934447992b
edy1482 <edward@ritchotte.com>
2024-12-22 12:12:02 +0000

    added comments and finished block.py

--- game/game_map.py
+++ game/game_map.py
@@ -2,15 +2,20 @@
 # map builds a game map, with random ore placement according to levels
 # The map is represented by a 2D list with each number representing a certain ore
 
-import block
+import blocks
 
 class game_map():
     """game_map"""
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.map = [[0] * width for _ in range(height)]
+        self.map = [[0] * width for _ in range(height)] #initializes the map with 0s
         
+    def build_map(self):
+        for y in range(self.width):
+            for x in range(self.height):
+                self.map[x][y] = blocks.ore.Dirt
+                
     def print(self):
         for row in self.map:
             print(row)
@@ -18,12 +23,13 @@ class game_map():
 
 
 
-# TESTING
+### TESTS ###
 
 def main():
-    test_map = game_map(5, 10)
+    test_map = game_map(30, 700)
     test_map.print()
-    print(block.ore(0))
+    test_map.build_map()
+    test_map.print()
 
 if __name__ == "__main__":
     main()
--- game/main.py
+++ game/main.py
@@ -1,6 +1,6 @@
 # This is the main loop for the game
 import pygame;
-import block, driller, game_map, main_menu, settings_menu
+import blocks, driller, game_map, main_menu, settings_menu
 
 
 def main():
